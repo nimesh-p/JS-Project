2571781e334e89190fdd9ff06d148e8c7bc0d109
nimesh-p <prajapatin953@gmail.com>
2021-02-07 16:42:18 -0800

    Second commit

--- JS/javascript.js
+++ JS/javascript.js
@@ -67,6 +67,10 @@ window.onload = function () {
             var cell5=newRow.insertCell(4);
             var cell6=newRow.insertCell(5);
             var cell7=newRow.insertCell(6);
+            var cell8=newRow.insertCell(7);
+            var cell9=newRow.insertCell(8);
+            var cell10=newRow.insertCell(9);
+
 
             cell1.innerHTML=sname;
             cell2.innerHTML=roll;
@@ -75,17 +79,76 @@ window.onload = function () {
             cell5.innerHTML=birthday;
             cell6.innerHTML=gender;
             cell7.innerHTML = '<input type="button" value="delete" onclick="deletedata(this);">';
+            cell8.innerHTML = '<input type="checkbox" class="checked">';
+            cell9.innerHTML = '<input type="button" value="Edit" onclick="SelectRow(this);">';
+            cell10.innerHTML ='<input type="button" value="Done" onclick="EditRow(this);">';
+  
             
 
 
 
-  };
+}
   function deletedata(btn) {
-    let sure = confirm("Do want to delete this row?");
+    let sure = confirm("Do you want to delete this row?");
     if (sure){
       var d = btn.parentNode.parentNode;
-      // console.log(btn.parentNode.parentNode);
+       console.log(btn.parentNode.parentNode);
       d.parentNode.removeChild(d);
-  } } 
-    
+  } 
+} 
+     
+
+
+
   
+//   function delMultiple(){
+//    let sure =confirm("Do you Want to Delete All record??");
+//    if(sure){ 
+//   var table = document.getElementById("tb1");
+// //or use :  var table = document.all.tableid;
+
+//     for(var i = table.rows.length - 1; i > 0; i--)
+//       {
+//          table.deleteRow(i);
+//       }
+//    }
+//  }
+function delMultiple() {
+  var sure = window.confirm("Are you sure you want to delete Selected records");
+  if(sure){
+    var checkedbox = document.querySelectorAll(".checked");
+    for(var i = 0; i<checkedbox.length; i++){
+      if(checkedbox[i].checked){
+        checkedbox[i].parentNode.parentNode.remove();
+      }
+    }
+  }
+}  
+
+
+ function SelectRow( newRow) {
+  document.getElementById('sname').value =  newRow.parentNode.parentNode.cells[0].innerHTML; 
+  document.getElementById('roll').value =  newRow.parentNode.parentNode.cells[1].innerHTML;
+  document.getElementById('dept').value =  newRow.parentNode.parentNode.cells[2].innerHTML; 
+  document.getElementById('year').value =  newRow.parentNode.parentNode.cells[3].innerHTML;
+  document.getElementById('birthday').value = newRow.parentNode.parentNode.cells[4].innerHTML; 
+  // document.querySelector('input[name="Gender"]').value = newRow.parentNode.parentNode.cells[5].innerHTML; 
+
+}
+
+function EditRow( newRow){
+  var sname = document.getElementById('sname').value;
+  var roll = document.getElementById('roll').value;
+  var dept = document.getElementById('dept').value;
+  var year = document.getElementById('year').value;
+  var birthday = document.getElementById('birthday').value;
+  // var gender= document.querySelector('input[name="Gender"]');
+
+  newRow.parentNode.parentNode.cells[0].innerHTML =sname;
+  newRow.parentNode.parentNode.cells[1].innerHTML = roll;
+  newRow.parentNode.parentNode.cells[2].innerHTML = dept;
+  newRow.parentNode.parentNode.cells[3].innerHTML = year;
+  newRow.parentNode.parentNode.cells[4].innerHTML = birthday;
+  // newRow.parentNode.parentNode.cells[5].innerHTML = gender;
+  
+}
